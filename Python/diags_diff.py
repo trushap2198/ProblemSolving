a7adeafc487713c61c3a17b2a16f7df66f84bc46
trusha <trushap2198@gmail.com>
2021-06-02 10:31:09 +0530

    Diagonals difference for NxN matrix

---
+++ Python/diags_diff.py
@@ -0,0 +1,30 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    principal,anti = 0,0
+    for i in range(len(arr)):
+        principal += arr[i][i]
+        anti += arr[n-i-1][i]
+    return abs(principal - anti)
+if __name__ == '__main__':
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+    print(result)
